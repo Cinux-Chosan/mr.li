36a264ac6a8a040950d8cb9e7635a5673726620f
Linsth <79124805+Linsth@users.noreply.github.com>
2021-02-16 16:19:50 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      // S.UI.simulate('#countdown 3');
+      S.UI.simulate('#countdown 3');
     }
 
     S.Drawing.loop(function () {
